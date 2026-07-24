9be91c9d7e57463e67685b9e91899c2ec02308e5
manu-pac <manuela@stoix.dev>
2026-07-24 17:20:41 -0300

    add alt_truths

---
+++ alt_truths.py
@@ -0,0 +1,56 @@
+#dado o set e o nome da pasta dele, criar um arquivo com varias listas 
+# (uma com a verdade de cada exemplo em cada mundo) - usando as funcoes 
+# de check e as funcoes de criaçao p reconstruir
+import argparse
+from pathlib import Path
+import classes as cl
+import tf_generation as tfg
+import pickle
+
+def main():
+    p = argparse.ArgumentParser()
+    p.add_argument("--folder",required=True)
+    p.add_argument("--n_worlds", type=int,help="number of worlds we want the truth of the formulas to be calculated to")
+
+    args = p.parse_args()
+
+    project_root = Path(__file__).resolve().parent
+    folder = project_root / "dataset" / args.folder
+
+    act_world = pickle.load(open(folder / "act_world.pkl", "rb"))
+    alt_worlds = pickle.load(open(folder / "alt_worlds.pkl", "rb"))
+    alt_worlds = sorted(alt_worlds)
+
+    dev_t = pickle.load(open(folder / "dev_t.pkl", "rb"))
+    dev_f = pickle.load(open(folder / "dev_f.pkl", "rb"))
+
+    number_pl, min_depth, max_depth, corpus_size, prop_td, n_worlds = pickle.load(
+        open(folder / "params.pkl", "rb")
+    )
+    tfg.setup(number_pl_=number_pl, max_depth_=max_depth, act_world_=act_world, alt_worlds_=alt_worlds)
+
+    w_truths = {"dev_t":{},"dev_f":{}}
+
+    for w in range(args.n_worlds):
+        world = alt_worlds[w]
+        i_func = cl.InterpretationFunc(set(world))
+        w_truths["dev_t"][world] = []
+        w_truths["dev_f"][world] = []
+
+        for i in dev_t:
+            f = tfg.true_le(i)
+            truth = f.check(i_func)
+            w_truths["dev_t"][world].append(truth)
+        for i in dev_f:
+            f = tfg.false_le(i)
+            truth = f.check(i_func)
+            w_truths["dev_f"][world].append(truth)
+
+ 
+    mean_path = project_root/ "dataset" / args.folder / "alt_truths.pkl"
+    with open(mean_path, "wb") as f:
+        pickle.dump(w_truths, f)
+    print(f"Saved alternative world truths to dataset/{args.folder}/alt_truths.pkl")
+
+if __name__ == "__main__":
+    main()
